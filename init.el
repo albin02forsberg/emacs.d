d60e25d8ccb28c3d720f765e383805c84fdfd7c9
Albin Forsberg <albin02forsberg@gmail.com>
2024-10-29 21:50:23 +0100

    Trying without evil...

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
  '(package-selected-packages
-   '(magit lsp-mode company tree-sitter org-bullets doom-modeline dashboard all-the-icons-ivy-rich ivy all-the-icons)))
+	 '(magit lsp-mode company tree-sitter org-bullets doom-modeline dashboard all-the-icons-ivy-rich ivy all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
